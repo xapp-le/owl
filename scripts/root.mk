83493295495bbc85437e5beb3be970bf7bd916a8
Yixun Lan <yixun.lan@gmail.com>
2016-01-12 16:55:14 +0800

    Cherry-pick back the previous commit# a8d7fa1d9a7f353ec4613febf30f4ca99a10a106:
    Makefile: slightly adjust variables name for modules_install

--- scripts/root.mk
+++ scripts/root.mk
@@ -33,7 +33,7 @@ CARD_BURN_UDISK_DIR=$(CARD_BURN_DIR)/card_burn_udsik
 CARD_BURN_IMAGE=$(CARD_BURN_DIR)/card_burn_image
 KERNEL_OUT_DIR=$(OUT_DIR)/kernel
 CARD_BURN_KERNEL_OUT_DIR=$(CARD_BURN_DIR)/kernel
-INSTALL_MOD_PATH=output
+INSTALL_MOD_PATH=$(OUT_DIR)/modules
 UBOOT_OUT_DIR=$(OUT_DIR)/u-boot
 K_BLD_CONFIG=$(KERNEL_OUT_DIR)/.config
 
@@ -70,7 +70,7 @@ modules: $(K_BLD_CONFIG)
 
 modules_install: $(K_BLD_CONFIG)
 	$(Q)$(MAKE) -C $(KERNEL_SRC) CROSS_COMPILE=$(CROSS_COMPILE) ARCH=$(ARCH) O=$(KERNEL_OUT_DIR) -j$(CPUS) INSTALL_MOD_PATH=$(INSTALL_MOD_PATH) modules_install
-	$(Q)cd $(KERNEL_OUT_DIR)/$(INSTALL_MOD_PATH) && tar czf modules.tgz lib
+	$(Q)cd $(INSTALL_MOD_PATH) && mkdir -p $(BURN_DIR)/ && tar czf $(BURN_DIR)/modules.tgz lib
 
 kernel-config: $(K_BLD_CONFIG)
 	$(Q)$(MAKE) -C $(KERNEL_SRC) ARCH=$(ARCH) O=$(KERNEL_OUT_DIR) menuconfig
@@ -140,7 +140,6 @@ burn_card: $(BURN_UDISK_SIZE)
 firmware: bootloader misc recovery 
 	$(Q)mkdir -p $(BURN_DIR)
 	$(Q)cp $(BURN_SRC)/burn.bin $(BURN_DIR)/
-	$(Q)cp $(KERNEL_OUT_DIR)/$(INSTALL_MOD_PATH)/modules.tgz $(BURN_DIR)/
 	$(Q)cp $(UBOOT_OUT_DIR)/u-boot-dtb.img $(IMAGE_DIR)/
 	$(Q)cp $(BOOTLOAD_DIR)/bootloader.bin $(IMAGE_DIR)/
 	
